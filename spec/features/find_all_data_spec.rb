c24938695edce9dadcd72f7cb0c896183bd1b067
Ian Whitney <whit0694@umn.edu>
2014-12-18 16:47:54 -0600

    Add feature specs for end-to-end tests
    These tests exercise the features of the gem that are outlined in the
    Readme. They use a simple Movie class that can return a collection of
    randomly-generated movie instances that can be used as a test data set.

---
+++ spec/features/find_all_data_spec.rb
@@ -0,0 +1,15 @@
+require_relative "../../lib/api_service_searching"
+require_relative "../fixtures/movie"
+
+RSpec.describe ApiServiceSearching, :focus do
+  let(:data_set) { Movie.random_collection }
+
+  it "A search with no query string returns all members of the data set" do
+    returned = ApiServiceSearching.where(
+      data_set,
+      nil
+    )
+
+    expect(returned).to eq(data_set)
+  end
+end
