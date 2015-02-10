97ff95e9a6a632011138a91cf1887f2e2c003dd2
Ian Whitney <whit0694@umn.edu>
2015-02-10 15:18:25 -0600

    Implement OR searches
    Implements this feature:
    https://github.com/umn-asr/query_string_search/issues/2

---
+++ spec/lib/query_string_search/matchers/match_multiple_attribute_values_spec.rb
@@ -0,0 +1,40 @@
+require_relative "../../../../lib/query_string_search/abstract_matcher"
+require_relative "../../../doubles/search_target"
+
+RSpec.describe MatchMultipleAttributeValues do
+  describe "match?" do
+    it "matches if the target's attribute is one of the values" do
+      target = Target.new
+      target.search_attr = "1994"
+      matcher = MatchMultipleAttributeValues.new(:search_attr, "1994|1995")
+      expect(matcher.match?(target)).to be_truthy
+    end
+
+    it "does not match if the target's attribute is not one of the values" do
+      target = Target.new
+      target.search_attr = "199"
+      matcher = MatchMultipleAttributeValues.new(:search_attr, "1994|1995")
+      expect(matcher.match?(target)).to be_falsey
+    end
+  end
+
+  describe "build_me?" do
+    describe "given a search type and search param that contains a pipe" do
+      it "is true" do
+        expect(MatchMultipleAttributeValues.build_me?(rand, "x|y")).to be_truthy
+      end
+    end
+
+    describe "given a search type and search param that starts with a pipe" do
+      it "is false" do
+        expect(MatchMultipleAttributeValues.build_me?(rand, "|x|y")).to be_falsey
+      end
+    end
+
+    describe "given a search type without a pipe" do
+      it "is false" do
+        expect(MatchMultipleAttributeValues.build_me?(rand, "xy")).to be_falsey
+      end
+    end
+  end
+end
