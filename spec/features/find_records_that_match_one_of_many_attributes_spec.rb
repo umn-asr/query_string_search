5bd677b3af0ec228faed39867b640589ec263fb4
Ian Whitney <whit0694@umn.edu>
2015-02-10 15:21:34 -0600

    Rubocop suggestion

--- spec/features/find_records_that_match_one_of_many_attributes_spec.rb
+++ spec/features/find_records_that_match_one_of_many_attributes_spec.rb
@@ -3,7 +3,7 @@ require_relative "../fixtures/movie"
 
 RSpec.describe "Finding data that matches one of many attributes" do
   let(:data_set) { Movie.random_collection }
-  let(:movies_in_1994_or_1995) { data_set.select { |d| ["1994", "1995"].include?(d.year) } }
+  let(:movies_in_1994_or_1995) { data_set.select { |d| %w(1994 1995).include?(d.year) } }
 
   it "Returns records that match the requested value" do
     results = QueryStringSearch.new(data_set, "year=1994|1995").results
