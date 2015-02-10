e9ea906482da58fde8d0e080e5e9686b4fb059fa
Ian Whitney <whit0694@umn.edu>
2015-02-10 13:21:06 -0600

    Feature test for OR matching
    New feature test to cover the OR matcher functionality described in the
    Readme.

---
+++ spec/features/find_records_that_match_one_of_many_attributes_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../../lib/query_string_search"
+require_relative "../fixtures/movie"
+
+RSpec.describe "Finding data that matches one of many attributes" do
+  let(:data_set) { Movie.random_collection }
+  let(:movies_in_1994_or_1995) { data_set.select { |d| ["1994", "1995"].include?(d.year) } }
+
+  it "Returns records that match the requested value" do
+    results = QueryStringSearch.new(data_set, "year=1994|1995").results
+
+    expect(results).to eq(movies_in_1994_or_1995)
+  end
+end
