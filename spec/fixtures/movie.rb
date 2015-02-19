abc07fefdf4e92bbea9de7f7b2aa01b1731fd140
Ian Whitney <whit0694@umn.edu>
2015-02-19 13:11:41 -0600

    Refactor creation of random movie

--- spec/fixtures/movie.rb
+++ spec/fixtures/movie.rb
@@ -2,17 +2,19 @@ class Movie
   attr_accessor :title, :rating, :year, :country, :seen, :star_rating
 
   def self.random_collection(count = 1_000)
-    (1...count).inject([]) do |collection, _|
-      collection << Movie.new(random_title, random_rating, random_year, random_country)
-    end
+    (1...count).map { |_| Movie.random }
   end
 
-  def initialize(title, rating, year, country)
-    self.title   = title
-    self.rating  = rating
-    self.year    = year
-    self.country = country
-    self.seen    = [true, false].sample
+  def self.random
+    Movie.new
+  end
+
+  def initialize
+    self.title       = random_title
+    self.rating      = random_rating
+    self.year        = random_year
+    self.country     = random_country
+    self.seen        = [true, false].sample
     self.star_rating = [1, 2, 3, 4, 5].sample
   end
 
@@ -20,19 +22,19 @@ class Movie
     seen
   end
 
-  def self.random_title
+  def random_title
     "Random Movie #{rand(10_000)}"
   end
 
-  def self.random_rating
+  def random_rating
     ["X", "NC-17", "R", "PG-13", "PG", "G", nil].sample
   end
 
-  def self.random_year
+  def random_year
     (1990..2014).to_a.sample.to_s
   end
 
-  def self.random_country
+  def random_country
     %w(
       US CAN UK HK BR RUS FR IN
     ).sample
