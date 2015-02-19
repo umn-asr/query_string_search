5889e8543d6543fff31a29deeb3146b5f311ad9e
Ian Whitney <whit0694@umn.edu>
2015-02-19 15:03:44 -0600

    name what we're building, not some vague term

--- lib/query_string_search/matcher_factory.rb
+++ lib/query_string_search/matcher_factory.rb
@@ -1,10 +1,10 @@
 module QueryStringSearch
   class MatcherFactory
     def self.build(search_option, build_options)
-      constructor = build_options.detect { |m| m.build_me?(search_option) }
+      matcher_to_build = build_options.detect { |m| m.build_me?(search_option) }
 
-      if constructor
-        constructor.new(search_option.search_type, search_option.search_param)
+      if matcher_to_build
+        matcher_to_build.new(search_option.search_type, search_option.search_param)
       end
     end
   end
