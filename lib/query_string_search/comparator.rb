e344f92f5434773a354bde0bce39032e054f9382
Ian Whitney <whit0694@umn.edu>
2015-02-20 07:58:45 -0600

    Move Comparator under the QueryStringSearch namespace

--- lib/query_string_search/comparator.rb
+++ lib/query_string_search/comparator.rb
@@ -1,29 +1,31 @@
-module Comparator
-  def self.does(subject)
-    Comparison.new(subject)
-  end
-
-  class Comparison
-    attr_accessor :subject
-
-    def initialize(subject)
-      self.subject = subject
+module QueryStringSearch
+  module Comparator
+    def self.does(subject)
+      Comparison.new(subject)
     end
 
-    def equal?(other)
-      normalize(subject) == normalize(other)
-    end
+    class Comparison
+      attr_accessor :subject
 
-    def normalize(unnormalized)
-      if unnormalized.respond_to?(:each)
-        unnormalized.map(&:to_s).map(&:upcase)
-      else
-        unnormalized.to_s.upcase
+      def initialize(subject)
+        self.subject = subject
       end
-    end
 
-    def contain?(other)
-      normalize(subject).include?(normalize(other))
+      def equal?(other)
+        normalize(subject) == normalize(other)
+      end
+
+      def normalize(unnormalized)
+        if unnormalized.respond_to?(:each)
+          unnormalized.map(&:to_s).map(&:upcase)
+        else
+          unnormalized.to_s.upcase
+        end
+      end
+
+      def contain?(other)
+        normalize(subject).include?(normalize(other))
+      end
     end
   end
 end
